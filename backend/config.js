4aaf18501ef5a4f0810a99f12c4b7e26b548a32a
awwaloyetunde <oyetundeawwal@yahoo.com>
2024-02-15 13:00:43 -0600

    first commit

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb+srv://root:root001@mern-project001.zdqt9ci.mongodb.net/?retryWrites=true&w=majority'
---
+++ frontend/src/pages/CreateBooks.jsx
@@ -0,0 +1,81 @@
+import React, { useState } from 'react';
+import BackButton from '../components/BackButton';
+import Spinner from '../components/Spinner';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { useSnackbar } from 'notistack';
+
+const CreateBooks = () => {
+    const [title, setTitle] = useState('');
+    const [author, setAuthor] = useState('');
+    const [publishYear, setPublishYear] = useState('');
+    const [loading, setLoading] = useState(false);
+    //To navigate to the main page after creating a new book
+    const navigate = useNavigate();
+    //TO use snackbar-notistack
+    const { enqueueSnackbar } = useSnackbar();
+    // To handle our book creating operation
+    const handleSaveBook = () => {
+        const data = {
+            title,
+            author,
+            publishYear,
+        };
+        setLoading(true);
+        axios
+            .post('http://localhost:5555/books', data)
+            .then(() => {
+                setLoading(false);
+                enqueueSnackbar('Book created sucessfully', { variant: 'sucess' });
+                navigate('/');
+            })
+            .catch((error) => {
+                setLoading(false);
+                //alert('An error occured. Please check console');
+                enqueueSnackbar('Error', { variant: 'error' });
+                console.log(error);
+            });
+    };
+
+    return (
+        <div claasName='p-4'>
+            <BackButton />
+            <h1 className='text-3x1 my-4'>Create Book</h1>
+            {loading ? <Spinner /> : ''}
+            <div className='flex flex-col border-2 border-sky-400 round-x1 w-[600px] p-4 mx-auto'>
+                <div className='my-4'>
+                    <label className='text-x1 mr-4 text-gray-500'>Title</label>
+                    <input
+                        type='text'
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                        className='border-2 border-gray-500 px-4 py-2 w-full'    
+                    />
+                </div>
+                <div className='my-4'>
+                    <label className='text-x1 mr-4 text-gray-500'>Author</label>
+                    <input
+                        type='text'
+                        value={author}
+                        onChange={(e) => setAuthor(e.target.value)}
+                        className='border-2 border-gray-500 px-4 py-2 w-full'    
+                    />
+                </div>
+                <div className='my-4'>
+                    <label className='text-x1 mr-4 text-gray-500'>Publish Year</label>
+                    <input
+                        type='number'
+                        value={publishYear}
+                        onChange={(e) => setPublishYear(e.target.value)}
+                        className='border-2 border-gray-500 px-4 py-2 w-full'    
+                    />
+                </div>
+                <button className='p-2 bg-sky-300 m-8' onClick={handleSaveBook}>
+                    Save
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default CreateBooks
